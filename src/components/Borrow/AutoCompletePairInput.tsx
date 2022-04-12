3bfb5b0948839cdcf5db01ac6ad618c3e91acde4
brian <quantum34@protonmail.com>
2022-04-12 20:58:23 +0900

    Add search component

--- src/components/Borrow/AutoCompletePairInput.tsx
+++ src/components/Borrow/AutoCompletePairInput.tsx
@@ -1,5 +1,6 @@
-import { FormControl, styled, Stack, Box, Select, MenuItem, Autocomplete, TextField } from '@mui/material'
+import { FormControl, styled, Stack, Box, Select, MenuItem, Autocomplete, TextField, InputAdornment } from '@mui/material'
 import Image from 'next/image'
+import SearchIcon from 'public/images/search-icon.png'
 
 interface AssetType {
 	tickerIcon: string
@@ -17,47 +18,84 @@ interface Props {
 
 const SelectPairInput: React.FC<Props> = ({ assets, selAssetId, onChangeAsset }) => {
 	return (
-		<FormControl variant="standard" sx={{ width: '100%' }}>
-			<FormStack direction="row" justifyContent="space-between" alignItems="center">
-        <Autocomplete
-          options={assets}
-          sx={{ width: 315 }}
-          selectOnFocus
-          clearOnBlur
-          freeSolo
-          onChange={(e) => onChangeAsset(e.target.value)}
-          renderInput={(params) => <TextField {...params} label="Search for an iAsset" />}
-        />
+		<Box>
+      <StyledAutocomplete
+        selectOnFocus
+        clearOnBlur
+        freeSolo
+        onChange={(e) => onChangeAsset(e.target.value)}
+        options={assets.map((asset) => asset.tickerName)}
+        renderInput={(params) => <StyledTextField {...params} label="Search for an iAsset" />}
+      />
 
-				{/* <Select
-					sx={{ color: '#fff' }}
-					id="selectAsset"
-					label=""
-					value={selAssetId}
-					onChange={(e) => onChangeAsset(e.target.value)}>
-					{assets.map((asset, index) => (
-						<MenuItem key={index} value={index}>
-							<Box display="flex">
-								<Image src={asset.tickerIcon} width="28px" height="28px" />
-								<Box sx={{ width: '100px', marginLeft: '8px', textAlign: 'left' }}>
-									<TickerSymbol>{asset.tickerSymbol}</TickerSymbol>
-								</Box>
-							</Box>
-						</MenuItem>
-					))}
-				</Select> */}
-			</FormStack>
-		</FormControl>
+      {/* <Select
+        sx={{ color: '#fff' }}
+        id="selectAsset"
+        label=""
+        value={selAssetId}
+        onChange={(e) => onChangeAsset(e.target.value)}>
+        {assets.map((asset, index) => (
+          <MenuItem key={index} value={index}>
+            <Box display="flex">
+              <Image src={asset.tickerIcon} width="28px" height="28px" />
+              <Box sx={{ width: '100px', marginLeft: '8px', textAlign: 'left' }}>
+                <TickerSymbol>{asset.tickerSymbol}</TickerSymbol>
+              </Box>
+            </Box>
+          </MenuItem>
+        ))}
+      </Select> */}
+    </Box>
 	)
 }
 
 const FormStack = styled(Stack)`
 	display: flex;
-	width: 100%;
+	width: 315px;
 	height: 54px;
-	padding: 13px 21px 13px 30px;
+	padding-left: 20px;
+  padding-right: 20px;
 	border-radius: 8px;
-	background-color: #333333;
+  border: solid 1px #809cff;
+  background-color: #151618;
+  font-size: 11px;
+  font-weight: 500;
+`
+
+const StyledAutocomplete = styled(Autocomplete)`
+  width: 315px; 
+  height: 36px;
+  border: solid 1px #809cff;
+  border-radius: 8px;
+  & label {
+    font-size: 11px;
+    font-weight: 500;
+    color: #989898;
+    padding: 0;
+    line-height: 3px;
+    overflow: initial;
+  }
+
+  & .MuiOutlinedInput-root {
+    padding: 0;
+    padding-left: 20px;
+    padding-right: 20px;
+  }
+
+  & .MuiAutocomplete-inputRoot {
+    padding: 0;
+    padding-left: 20px;
+  }
+
+  & input {
+    width: 315px;
+    height: 24px;
+    color: #989898;
+  }
+`
+
+const StyledTextField = styled(TextField)`
+  line-height: 3px;
 `
 
 const TickerSymbol = styled('div')`
--- src/components/Liquidity/StatusView.tsx
+++ src/components/Liquidity/StatusView.tsx
@@ -1,4 +1,5 @@
-import { styled, Box, Paper, Stack } from '@mui/material'
+import { styled, Box, Paper, Stack, Tabs, Tab } from '@mui/material'
+import { useState } from 'react'
 import { withCsrOnly } from '~/hocs/CsrOnly'
 import { Status } from '~/web3/MyLiquidity/status'
 
@@ -7,34 +8,53 @@ interface Props {
 }
 
 const StatusView: React.FC<Props> = ({ status }) => {
+  const [tab, setTab] = useState(0)
+
+  const handleChangeTab = (event: React.SyntheticEvent, newValue: number) => {
+		setTab(newValue)
+	}
+
 	return status ? (
 		<StyledPaper variant="outlined">
-			<Box sx={{ marginBottom: '40px' }}>
-				<Title>Total Value</Title>
-				<BalanceValue>
-					<NumValue>{status.totalVal.toLocaleString()}</NumValue> USDi
-				</BalanceValue>
-			</Box>
-			{/* <Stack spacing={2}>
-				<Box>
-					<SubHeader>Comet:</SubHeader>
-					<SubValue>
-						{status.comet.toLocaleString()} USDi ({status.cometPercent}%)
-					</SubValue>
-				</Box>
-				<Box>
-					<SubHeader>Unconcentrated:</SubHeader>
-					<SubValue>
-						{status.unconcentrated.toLocaleString()} USDi ({status.unconcentratedPercent}%)
-					</SubValue>
-				</Box>
-				<Box>
-					<SubHeader>Borrow:</SubHeader>
-					<SubValue>
-						{status.borrow.toLocaleString()} USDi ({status.borrowPercent}%)
-					</SubValue>
-				</Box>
-			</Stack> */}
+      <StyledTabs value={tab} onChange={handleChangeTab}>
+        <StyledTab value={0} label="All" />
+        <StyledTab value={1} label="Comet" />
+        <StyledTab value={2} label="Unconcentrated" />
+        <StyledTab value={3} label="Borrow" />
+      </StyledTabs>
+
+      <TabPanel value={tab} index={0}>
+        <Box>
+          <Title>Total Value</Title>
+          <BalanceValue>
+            <NumValue>{status.totalVal.toLocaleString()}</NumValue> USDi
+          </BalanceValue>
+        </Box>
+      </TabPanel>
+      <TabPanel value={tab} index={1}>
+        <Box>
+          <Title>Comet</Title>
+          <BalanceValue>
+            <NumValue>{status.comet.toLocaleString()}</NumValue> USDi
+          </BalanceValue>
+        </Box>
+      </TabPanel>
+      <TabPanel value={tab} index={2}>
+        <Box>
+          <Title>Unconcentrated</Title>
+          <BalanceValue>
+            <NumValue>{status.unconcentrated.toLocaleString()}</NumValue> USDi
+          </BalanceValue>
+        </Box>
+      </TabPanel>
+      <TabPanel value={tab} index={3}>
+        <Box>
+          <Title>Borrow</Title>
+          <BalanceValue>
+            <NumValue>{status.borrow.toLocaleString()}</NumValue> USDi
+          </BalanceValue>
+        </Box>
+      </TabPanel>
 		</StyledPaper>
 	) : (
 		<></>
@@ -46,9 +66,9 @@ export default withCsrOnly(StatusView)
 const StyledPaper = styled(Paper)`
 	font-size: 14px;
 	font-weight: 500;
-	background-image: linear-gradient(to bottom, rgba(21, 22, 24, 0.75) 100%, #002888 -159%);
+	background: linear-gradient(to bottom, rgba(21, 22, 24, 0.75) 100%, #002888 -159%);
 	color: #fff;
-	padding: 49px 48px 49px 51px;
+	padding: 10px 18px;
 	border-radius: 10px;
 	height: 168px;
 `
@@ -60,7 +80,7 @@ const Title = styled('div')`
 `
 
 const BalanceValue = styled('div')`
-	font-size: 24px;
+	font-size: 14px;
 	font-weight: 500;
   font-stretch: normal;
   font-style: normal;
@@ -69,15 +89,83 @@ const BalanceValue = styled('div')`
 `
 
 const NumValue = styled('span')`
-	font-size: 32px;
+	font-size: 24px;
 `
 
-const SubHeader = styled('div')`
-	font-size: 12px;
-	color: #989898;
-`
+interface TabPanelProps {
+	children?: React.ReactNode
+	index: number
+	value: number
+}
 
-const SubValue = styled('div')`
-	font-size: 13px;
-	font-weight: 500;
-`
+interface StyledTabsProps {
+	children?: React.ReactNode
+	value: number
+	onChange: (event: React.SyntheticEvent, newValue: number) => void
+}
+
+interface StyledTabProps {
+	label: string
+}
+
+const StyledTabs = styled((props: StyledTabsProps) => (
+	<Tabs {...props} TabIndicatorProps={{ children: <span className="MuiTabs-indicatorSpan" /> }} />
+))({
+	'& .MuiTabs-indicator': {
+		display: 'flex',
+		justifyContent: 'center',
+		backgroundColor: 'transparent',
+	},
+	'& .MuiTabs-indicatorSpan': {
+		display: 'none'
+	},
+  maxWidth: '620px',
+  borderRadius: '10px',
+  height: '47px',
+  paddingLeft: '8px',
+  paddingTop: '6px'
+})
+
+const StyledTab = styled((props: StyledTabProps) => <Tab disableRipple iconPosition="start" {...props} />)(({ theme }) => ({
+  '&.MuiTab-root': {
+    height: '35px',
+    minHeight: '0px',
+    maxHeight: '35px',
+    display: 'flex',
+    gap: '10px'
+  },
+  textTransform: 'none',
+	fontWeight: '600',
+	fontSize: '12px',
+  marginLeft: '6px',
+  marginRight: '6px',
+	color: '#989898',
+	'&.Mui-selected': {
+    border: 'solid 1px #3f3f3f',
+    backgroundColor: '#000',
+		color: '#fff',
+    borderRadius: '10px'
+	},
+	'&.Mui-focusVisible': {
+		backgroundColor: '#3d3d3d',
+	},
+}))
+
+const TabPanel = (props: TabPanelProps) => {
+	const { children, value, index, ...other } = props
+
+	return (
+		<div
+			role="tabpanel"
+			hidden={value !== index}
+			id={`simple-tabpanel-${index}`}
+			aria-labelledby={`simple-tab-${index}`}
+			{...other}>
+			{value === index && (
+				<Box sx={{ px: 3, py: 2 }}>
+					<div>{children}</div>
+				</Box>
+			)}
+		</div>
+	)
+}
