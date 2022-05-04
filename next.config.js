99463066dc02ffed5311a3561a8c13f63abb9ee0
brian <quantum34@protonmail.com>
2022-05-05 08:22:49 +0900

    set external cofig

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
-  }
+  },
+  experimental: {
+    externalDir: true,
+  },
 }
 
 module.exports = nextConfig
