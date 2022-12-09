2fd97712fd9a023d14a163cc3373df8435640231
brian <quantum34@protonmail.com>
2022-12-09 10:23:39 +0900

    Add mutation for callRecenterAll

--- src/features/MyLiquidity/multipool/Recenter.mutation.ts
+++ src/features/MyLiquidity/multipool/Recenter.mutation.ts
@@ -3,6 +3,35 @@ import { Incept } from "incept-protocol-sdk/sdk/src/incept"
 import { useMutation } from 'react-query'
 import { useIncept } from '~/hooks/useIncept'
 
+export const callRecenterAll = async ({
+  program,
+  userPubKey,
+	data
+}: CallRecenterProps) => {
+  if (!userPubKey) throw new Error('no user public key')
+
+  console.log('recenter data', data)
+
+  const { poolIndex } = data
+
+  await program.loadManager()
+
+  // @TODO: Call to recenterAll
+  // await program.recenterComet(
+	// 	poolIndex,
+	// 	[]
+	// )
+
+  return {
+    result: true
+  }
+}
+
+export function useRecenterAllMutation(userPubKey : PublicKey | null ) {
+  const { getInceptApp } = useIncept()
+  return useMutation((data: RecenterFormData) => callRecenterAll({ program: getInceptApp(), userPubKey, data }))
+}
+
 export const callRecenter = async ({
   program,
   userPubKey,
