95533d1f2fd46bf327b86f7731e7ce092ad5ba1d
AronYeager <100383952+AronYeager@users.noreply.github.com>
2023-01-03 21:50:27 -0500

    Fix/core 352 (#50)
    * Filtering on current positions
    
    * running prettier
    
    * catching undefined value
    
    Co-authored-by: brian <quantum34@protonmail.com>

--- src/features/MyLiquidity/multipool/LiquidityPools.query.ts
+++ src/features/MyLiquidity/multipool/LiquidityPools.query.ts
@@ -1,78 +1,101 @@
-import { Query, useQuery } from 'react-query'
-import { PublicKey } from '@solana/web3.js'
-import { Incept } from 'incept-protocol-sdk/sdk/src/incept'
-import { useIncept } from '~/hooks/useIncept'
-import { useDataLoading } from '~/hooks/useDataLoading'
-import { REFETCH_CYCLE } from '~/components/Common/DataLoadingIndicator'
-import { getiAssetInfos } from '~/utils/assets'
-import { assetMapping } from '~/data/assets'
+import { Query, useQuery } from "react-query"
+import { PublicKey } from "@solana/web3.js"
+import { Incept } from "incept-protocol-sdk/sdk/src/incept"
+import { useIncept } from "~/hooks/useIncept"
+import { useDataLoading } from "~/hooks/useDataLoading"
+import { REFETCH_CYCLE } from "~/components/Common/DataLoadingIndicator"
+import { getiAssetInfos } from "~/utils/assets"
+import { assetMapping } from "~/data/assets"
 
 export const fetchPools = async ({
-	program,
-	userPubKey,
-	setStartTimer,
+  program,
+  userPubKey,
+  setStartTimer,
 }: {
-	program: Incept
-	userPubKey: PublicKey | null
-	setStartTimer: (start: boolean) => void
+  program: Incept
+  userPubKey: PublicKey | null
+  setStartTimer: (start: boolean) => void
 }) => {
-	if (!userPubKey) return []
+  if (!userPubKey) return []
 
-	await program.loadManager()
+  await program.loadManager()
 
-	console.log('fetchPools :: LiquidityPools.query')
-	// start timer in data-loading-indicator
-	setStartTimer(false)
-	setStartTimer(true)
+  console.log("fetchPools :: LiquidityPools.query")
+  // start timer in data-loading-indicator
+  setStartTimer(false)
+  setStartTimer(true)
 
-	const tokenData = await program.getTokenData()
-	const assetInfos = getiAssetInfos(tokenData)
+  const [tokenDataResult, cometResult] = await Promise.allSettled([
+    program.getTokenData(),
+    program.getComet(),
+  ])
+  if (tokenDataResult.status === "rejected" || cometResult.status === "rejected") {
+    throw new Error("Couldn't fetch data!")
+  }
+  const tokenData = tokenDataResult.value
+  const comet = cometResult.value
+  const assetInfos = getiAssetInfos(tokenData)
 
-	let result = []
+  let currentPoolSet = new Set()
 
-	for (let asset of assetInfos) {
-		let { tickerIcon, tickerSymbol } = assetMapping(asset[0])
-		result.push({
-			id: asset[0],
-			tickerSymbol,
-			tickerIcon,
-			totalLiquidity: asset[2],
-			volume24H: 0,
-			averageAPY: 0,
-			isEnabled: true,
-		})
-	}
-	return result
+  for (let i = 0; i < Number(comet.numPositions); i++) {
+    const poolIndex = Number(comet.positions[i].poolIndex)
+    currentPoolSet.add(poolIndex)
+  }
+
+  let result = []
+
+  for (let asset of assetInfos) {
+    if (currentPoolSet.has(asset[0])) {
+      continue
+    }
+    let { tickerIcon, tickerSymbol } = assetMapping(asset[0])
+    result.push({
+      id: asset[0],
+      tickerSymbol,
+      tickerIcon,
+      totalLiquidity: asset[2],
+      volume24H: 0,
+      averageAPY: 0,
+      isEnabled: true,
+    })
+  }
+
+  return result
 }
 
 interface GetPoolsProps {
-	userPubKey: PublicKey | null
-	refetchOnMount?: boolean | "always" | ((query: Query) => boolean | "always")
-	enabled?: boolean
+  userPubKey: PublicKey | null
+  refetchOnMount?: boolean | "always" | ((query: Query) => boolean | "always")
+  enabled?: boolean
 }
 
 export interface PoolList {
-	id: number
-	tickerSymbol: string
-	tickerIcon: string
-	totalLiquidity: number
-	volume24H: number
-	averageAPY: number
-	isEnabled: boolean
+  id: number
+  tickerSymbol: string
+  tickerIcon: string
+  totalLiquidity: number
+  volume24H: number
+  averageAPY: number
+  isEnabled: boolean
 }
 
-export function useLiquidityPoolsQuery({ userPubKey, refetchOnMount, enabled = true }: GetPoolsProps) {
-	const { getInceptApp } = useIncept()
-	const { setStartTimer } = useDataLoading()
+export function useLiquidityPoolsQuery({
+  userPubKey,
+  refetchOnMount,
+  enabled = true,
+}: GetPoolsProps) {
+  const { getInceptApp } = useIncept()
+  const { setStartTimer } = useDataLoading()
 
-	return useQuery(
-		['liquidityPools', userPubKey],
-		() => fetchPools({ program: getInceptApp(), userPubKey, setStartTimer }),
-		{
-			refetchOnMount,
-			refetchInterval: REFETCH_CYCLE,
-			refetchIntervalInBackground: true,
-			enabled,
-		}
-	)
+  return useQuery(
+    ["liquidityPools", userPubKey],
+    () => fetchPools({ program: getInceptApp(), userPubKey, setStartTimer }),
+    {
+      refetchOnMount,
+      refetchInterval: REFETCH_CYCLE,
+      refetchIntervalInBackground: true,
+      enabled,
+    }
+  )
 }
