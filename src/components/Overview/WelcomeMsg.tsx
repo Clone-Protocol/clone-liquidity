bc9c35782f12efc141b42383b05b5c214a9b62a0
brian <quantum34@protonmail.com>
2023-01-11 14:43:57 +0900

    set common theme css

--- src/components/Overview/WelcomeMsg.tsx
+++ src/components/Overview/WelcomeMsg.tsx
@@ -1,23 +1,20 @@
 import Paper from '@mui/material/Paper'
 import Slide from '@mui/material/Slide';
-import { styled } from '@mui/material'
+import { styled, Typography } from '@mui/material'
 
 const WelcomeMsg = () => {
 	return (
-    <Slide direction="down" in={true} mountOnEnter unmountOnExit>
-      <StyledPaper variant="outlined">
-        👋 Hi and welcome to Incept Liquidity. Here is where anyone can be a liquidity provider
-        (LP) of the Incept Trading Platform and earn trading fees and rewards.
-      </StyledPaper>
-    </Slide>
+		<Slide direction="down" in={true} mountOnEnter unmountOnExit>
+			<StyledPaper variant="outlined">
+				<Typography variant="h8">👋 Hi and welcome to Incept Liquidity. Here is where anyone can be a liquidity provider
+					(LP) of the Incept Trading Platform and earn trading fees and rewards.</Typography>
+			</StyledPaper>
+		</Slide>
 	)
 }
 
 const StyledPaper = styled(Paper)`
-	font-size: 12px;
-	font-weight: 500;
 	font-stretch: normal;
-	font-style: normal;
 	line-height: normal;
 	letter-spacing: normal;
 	text-align: center;
