7b34762deb7bde6134c8729dbab87749eae97c43
brian <quantum34@protonmail.com>
2023-04-04 20:17:33 +0900

    update close panel

--- src/containers/Liquidity/comet/ManageComet.tsx
+++ src/containers/Liquidity/comet/ManageComet.tsx
@@ -75,7 +75,7 @@ const ManageComet = ({ assetId }: { assetId: string }) => {
             <EditPanel assetId={assetId} cometDetail={cometDetail} balance={usdiBalance.balanceVal} onRefetchData={() => refetch()} />
           </TabPanelForEdit>
           <TabPanelForEdit value={tab} index={1}>
-            <ClosePanel assetId={assetId} cometDetail={cometDetail} onRefetchData={() => refetch()} />
+            <ClosePanel assetId={assetId} cometDetail={cometDetail} balance={usdiBalance.balanceVal} onRefetchData={() => refetch()} />
           </TabPanelForEdit>
         </LeftBoxWrapper>
       </Box>
