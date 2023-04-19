d05ef1231f0595cbd4d3346a5f66f97dee00e679
Aron Yeager <aronyeager@protonmail.com>
2023-04-19 19:00:23 -0400

    Add in data fetching/cleaning logic

--- src/features/Chart/Liquidity.query.ts
+++ src/features/Chart/Liquidity.query.ts
@@ -1,37 +1,87 @@
 import { Query, useQuery } from 'react-query'
 import { FilterTime } from '~/components/Charts/TimeTabs'
+import { getGoogleSheetsDoc } from "~/utils/google_sheets"
+import moment from 'moment'
+import { DEVNET_TOKEN_SCALE } from 'incept-protocol-sdk/sdk/src/incept'
 
 export interface ChartElem {
   time: string
   value: number
 }
 
+type TimeSeriesValue = {time: string, value: number}
+const MAX_ITERATIONS = 100000;
+
+const getMomentFromTimeframe = (timeframe: FilterTime) => {
+  let daysLookback = (() => {
+    switch (timeframe) {
+      case '1y':
+        return 365
+      case '30d':
+        return 30
+      case '7d':
+        return 7
+      case '24h':
+        return 1
+      default:
+        throw new Error(`Unexpected timeframe: ${timeframe}`)
+    }
+  })()
+  return moment().utc(
+    ).subtract(daysLookback, 'days')
+}
+
+const fillInTimeGaps = (arr: {time: number, value: number}[], endTimestamp: number): TimeSeriesValue[] =>  {
+
+  let chartData: TimeSeriesValue[] = []
+
+  if (arr.length === 0)
+    return [{time: moment(endTimestamp * 1e3).utc().format(), value: 0}]
+
+  let index = 0
+  let startingTime = arr[0].time
+  let currentValue = arr[0].value
+
+  let N = Math.floor((endTimestamp - startingTime) / 3600)
+
+  for (let i = 0; i<N; i++) {
+    let currentTime = startingTime + 3600 * i
+    let {time, value} = arr[index]
+    if (currentTime >= time) {
+      currentValue = value
+      index = Math.min(index + 1, arr.length - 1)
+    }
+    chartData.push({time: moment(currentTime * 1e3).utc().format(), value: currentValue})
+  }
+
+  return chartData
+
+}
+
 export const fetchTotalLiquidity = async ({ timeframe } : { timeframe: FilterTime}) => {
-	const chartData = [
-    {
-      time: '2022-03-01',
-      value: 15
-    },
-    {
-      time: '2022-03-02',
-      value: 35
-    },
-    {
-      time: '2022-03-03',
-      value: 80
-    },
-    {
-      time: '2022-03-04',
-      value: 65
-    },
-    {
-      time: '2022-03-05',
-      value: 115
-    },
-  ]
+
+  let temp = [];
+
+  const doc = await getGoogleSheetsDoc()
+  
+  const sheet = await doc.sheetsByTitle["HourlyLiquidityDelta"]
+  await sheet.loadCells();
+
+  const startingTimestamp = getMomentFromTimeframe(timeframe).unix()
+
+  for (let row = 2; row < MAX_ITERATIONS; row++) {
+    let blockTime = sheet.getCell(row, 0).formattedValue
+    const usdiDelta = sheet.getCell(row, 2).formattedValue
+    if (blockTime === null || usdiDelta === null) 
+      break;
+    blockTime = Number(blockTime)
+
+    if (blockTime >= startingTimestamp)
+      temp.push({time: blockTime, value: usdiDelta * Math.pow(10, -DEVNET_TOKEN_SCALE)})
+  }
 
   return {
-    chartData,
+    chartData: fillInTimeGaps(temp, moment().utc().unix())
   }
 }
 
@@ -58,42 +108,65 @@ export const fetchTotalUsers = async ({ timeframe } : { timeframe: FilterTime}) 
       value: 235
     },
   ]
+  return { chartData }
 
-  return {
-    chartData,
-  }
+  // @TODO Fix later
+  // let temp = [];
+
+  // const doc = await getGoogleSheetsDoc()
+  
+  // const sheet = await doc.sheetsByTitle["HourlyUserCount"]
+  // await sheet.loadCells();
+
+  // const startingTimestamp = getMomentFromTimeframe(timeframe).unix()
+
+  // for (let row = 1; row < MAX_ITERATIONS; row++) {
+  //   let blockTime = sheet.getCell(row, 0).formattedValue
+  //   const cumulativeUsers = sheet.getCell(row, 2).formattedValue
+  //   console.log(blockTime, cumulativeUsers)
+  //   if (blockTime === null || cumulativeUsers === null) 
+  //     break;
+  //   blockTime = Number(blockTime)
+
+  //   if (blockTime >= startingTimestamp)
+  //     temp.push({time: blockTime, value: Number(cumulativeUsers)})
+  // }
+
+  // return {
+  //   chartData: fillInTimeGaps(temp, moment().utc().unix())
+  // }
 }
 
 export const fetchTotalVolume = async ({ timeframe } : { timeframe: FilterTime}) => {
-	const chartData = [
-    {
-      time: '2022-03-01',
-      value: 15
-    },
-    {
-      time: '2022-03-02',
-      value: 35
-    },
-    {
-      time: '2022-03-03',
-      value: 80
-    },
-    {
-      time: '2022-03-04',
-      value: 65
-    },
-    {
-      time: '2022-03-05',
-      value: 115
-    },
-  ]
+
+  let temp = []
+
+  const doc = await getGoogleSheetsDoc()
+  
+  const sheet = await doc.sheetsByTitle["HourlyTradingVolume"]
+  await sheet.loadCells();
+
+  const startingTimestamp = getMomentFromTimeframe(timeframe).unix()
+
+  for (let row = 2; row < MAX_ITERATIONS; row++) {
+    let blockTime = sheet.getCell(row, 0).formattedValue
+    const usdiDelta = sheet.getCell(row, 2).formattedValue
+    if (blockTime === null || usdiDelta === null) 
+      break;
+  
+    blockTime = Number(blockTime)
+
+    if (blockTime >= startingTimestamp)
+      temp.push({time: blockTime, value: usdiDelta * Math.pow(10, -DEVNET_TOKEN_SCALE)})
+  }
 
   return {
-    chartData,
+    chartData: fillInTimeGaps(temp, moment().utc().unix())
   }
 }
 
 export const fetchTotalLiquidation = async ({ timeframe } : { timeframe: FilterTime}) => {
+  // @TODO: When we setup the liquidation bot.
 	const chartData = [
     {
       time: '2022-03-01',
