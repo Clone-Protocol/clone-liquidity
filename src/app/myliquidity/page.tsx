5da81d303ed5edb8eb61e23fbc524be1dbaa5d0c
brian <quantum34@protonmail.com>
2023-07-25 17:53:57 +0900

    hotfix

--- src/app/myliquidity/page.tsx
+++ src/app/myliquidity/page.tsx
@@ -3,13 +3,15 @@ import { StyledSection } from '~/app/page'
 import { Container, Box } from '@mui/material'
 import LiquidityTable from '~/containers/Liquidity/LiquidityTable'
 
-const MyLiquidity = ({ params }: { params: { lTab: string } }) => {
+// const MyLiquidity = ({ params }: { params: { lTab: string } }) => {
+const MyLiquidity = () => {
 
   return (
     <StyledSection>
       <Container>
         <Box marginTop='40px'>
-          <LiquidityTable ltab={params.lTab} />
+          {/* <LiquidityTable ltab={params.lTab} /> */}
+          <LiquidityTable ltab={'0'} />
         </Box>
       </Container>
     </StyledSection>
