2d40cce847a4b505a3eb32da62c40e6efa42fca3
Aron Yeager <aronyeager@protonmail.com>
2023-10-31 10:12:16 -0400

    Add netlify fcns to supabase

--- src/utils/fetch_netlify.ts
+++ src/utils/fetch_netlify.ts
@@ -8,6 +8,61 @@ export const fetchFromCloneIndex = async (method: string, params: Object) => {
     return await axios.get(`/.netlify/functions/clone-index-fetch?${queryString}`)
 }
 
+export interface StatsData {
+    time_interval: string;
+    total_committed_collateral_liquidity: number;
+    volume: number;
+    trading_fees: number;
+    pool_index: number | undefined;
+}
+
+export const fetchStatsData = async (interval: string, filter: string, aggregated: boolean = false): Promise<StatsData[]> => {
+
+    let endpoint = (() => {
+        if (aggregated) {
+            return `/.netlify/functions/get-aggregated-stats?interval=${interval}&filter=${filter}`
+        } else {
+            return `/.netlify/functions/get-pool-stats?interval=${interval}&filter=${filter}`
+        }
+    })()
+
+    const response = await axios.get(endpoint)
+    return response.data as StatsData[]
+}
+
+export interface OHLCVResponse {
+    time_interval: string,
+    pool_index: number,
+    open: string,
+    high: string,
+    low: string,
+    close: string,
+    volume: string,
+    trading_fees: string
+  }
+
+export const fetchOHLCV = async (interval: string, filter: string, pool: string | null = null): Promise<OHLCVResponse[]> => {
+    let endpoint = `/.netlify/functions/get-ohlcv?interval=${interval}&filter=${filter}`
+
+    if (pool !== null) 
+        endpoint += `&pool=${pool}`
+
+    const response = await axios.get(endpoint)
+    return response.data as OHLCVResponse[]
+}
+
+export interface BorrowStats {
+    pool_index: number,
+    time_interval: string,
+    cumulative_collateral_delta: number,
+    cumulative_borrowed_delta: number
+}
+
+export const fetchBorrowStats = async (): Promise<BorrowStats[]> => {
+    const response = await axios.get(`/.netlify/functions/get-borrow-stats`)
+    return response.data as BorrowStats[]
+}
+
 export const fetchFromSupabaseNotice = async () => {
     return await axios.get(`/.netlify/functions/supabase-notice-fetch`)
 }
