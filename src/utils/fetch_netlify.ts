54e69096f3897ec98a59e268f4247f313051a477
brian <quantum34@protonmail.com>
2023-12-08 19:45:04 +0900

    add middelware for geoblocking

--- src/utils/fetch_netlify.ts
+++ src/utils/fetch_netlify.ts
@@ -39,12 +39,12 @@ export interface OHLCVResponse {
     close: string,
     volume: string,
     trading_fees: string
-  }
+}
 
 export const fetchOHLCV = async (interval: string, filter: string, pool?: number | string): Promise<OHLCVResponse[]> => {
     let endpoint = `/.netlify/functions/get-ohlcv?interval=${interval}&filter=${filter}`
 
-    if (pool !== undefined) 
+    if (pool !== undefined)
         endpoint += `&pool=${pool}`
 
     const response = await axios.get(endpoint)
@@ -63,9 +63,9 @@ export const fetchBorrowStats = async (): Promise<BorrowStats[]> => {
     return response.data as BorrowStats[]
 }
 
-export const fetchPoolApy = async (): Promise<{pool_index: number, apy_24hr: number}[]> => {
+export const fetchPoolApy = async (): Promise<{ pool_index: number, apy_24hr: number }[]> => {
     const response = await axios.get(`/.netlify/functions/get-pool-apy`)
-    return response.data as {pool_index: number, apy_24hr: number}[]
+    return response.data as { pool_index: number, apy_24hr: number }[]
 }
 
 export const fetchUserApy = async (user_address: string): Promise<number> => {
@@ -73,9 +73,9 @@ export const fetchUserApy = async (user_address: string): Promise<number> => {
     return response.data as number
 }
 
-export const fetchTotalLiquidity = async (interval: string, filter: string): Promise<{time_interval: string, total_liquidity: number}[]> => {
+export const fetchTotalLiquidity = async (interval: string, filter: string): Promise<{ time_interval: string, total_liquidity: number }[]> => {
     const response = await axios.get(`/.netlify/functions/get-total-liquidity?interval=${interval}&filter=${filter}`)
-    return response.data as {time_interval: string, total_liquidity: number}[]
+    return response.data as { time_interval: string, total_liquidity: number }[]
 }
 
 export const fetchFromSupabaseNotice = async () => {
@@ -90,7 +90,7 @@ export type UserPointsView = {
     social_points: number
     total_points: number
     name?: string
-  }
+}
 
 export const fetchUserPoints = async (userAddress?: string): Promise<UserPointsView[]> => {
     let url = `/.netlify/functions/get-user-points`;
@@ -101,3 +101,7 @@ export const fetchUserPoints = async (userAddress?: string): Promise<UserPointsV
     return response.data as UserPointsView[]
 }
 
+export const fetchGeoBlock = async () => {
+    const response = await axios.post(`/api`)
+    return response.data
+}
