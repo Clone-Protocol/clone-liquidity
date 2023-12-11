67e2e008d3b6b62733b69b9c0436c7f1fdf550b0
brian <quantum34@protonmail.com>
2023-12-11 14:16:37 +0900

    fix middleware for geoblock

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,17 @@
-import { Context } from "@netlify/edge-functions";
 import { NextRequest, NextResponse } from 'next/server'
 
-export async function middleware(req: NextRequest, context: Context) {
+export async function middleware(req: NextRequest) {
   const { nextUrl: url, geo } = req
 
   const BLOCKED_COUNTRY_CODE_ARRAY = [
     'AF', 'AS', 'AO', 'AM', 'AZ', 'BY', 'BA', 'BW', 'BI', 'KH', 'CM', 'CA', 'CF', 'TD', 'CN', 'KP', 'CD', 'ER', 'ET', 'GH', 'GU', 'GN', 'GW', 'HT', 'IR', 'IQ', 'JP', 'LA', 'LB', 'LR', 'LY', 'MG', 'ML', 'MZ', 'MM', 'NI', 'MP', 'PK', 'PR', 'CG', 'RU', 'SO', 'SD', 'UA', 'LK', 'SY', 'TJ', 'TT', 'TM', 'UG', 'US', 'UZ', 'VU', 'VE', 'VI', 'YE', 'ZW'
   ]
 
-  console.log('contextGeo', context.geo)
   console.log('geo', geo?.country)
 
-  const countryCode = context.geo?.country?.code;
-  const countryName = context.geo?.country?.name;
-  // console.log('geo.country', geo?.country)
-  console.log('countryCode', countryCode)
-  console.log('countryName', countryName)
   // url.searchParams.set('country', geo?.country)
 
-  if (BLOCKED_COUNTRY_CODE_ARRAY.includes(countryCode)) {
+  if (geo && BLOCKED_COUNTRY_CODE_ARRAY.includes(geo.country!)) {
     return NextResponse.json({
       result: false
     })
