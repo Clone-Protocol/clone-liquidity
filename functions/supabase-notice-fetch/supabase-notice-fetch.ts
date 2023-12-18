0b21c627afffb657aa112e148932c6640d9c7fb8
brian <quantum34@protonmail.com>
2023-12-18 16:33:04 +0900

    update notice for mainnet

--- functions/supabase-notice-fetch/supabase-notice-fetch.ts
+++ functions/supabase-notice-fetch/supabase-notice-fetch.ts
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 import { Handler } from '@netlify/functions'
+import { IS_DEV } from '~/data/networks'
 
 export const handler: Handler = async (event, context) => {
   const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
@@ -8,7 +9,7 @@ export const handler: Handler = async (event, context) => {
     const { data, error } = await supabase
       .from('notices')
       .select()
-      .eq('channel', 'liquidity')
+      .eq('channel', IS_DEV ? 'liquidity' : 'liquidity-main')
       .eq('show', true)
 
     return {
