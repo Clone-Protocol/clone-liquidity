812fe43233ad899daf00e6adc8acdf40d94ed23c
brian <99372590+qtum34@users.noreply.github.com>
2023-12-21 11:29:24 +0900

    Revert "finish point dashboard"
    This reverts commit fbdf11670413671320b8b43fb3eef01b23131003.

--- src/containers/Points/MyPointStatus.tsx
+++ src/containers/Points/MyPointStatus.tsx
@@ -1,86 +1,69 @@
 import { styled } from '@mui/system'
-import { Box, Button, Stack, Typography } from '@mui/material'
+import { Box, Stack, Typography } from '@mui/material'
 import InfoTooltip from '~/components/Common/InfoTooltip'
 import { TooltipTexts } from '~/data/tooltipTexts'
 import { RankIndex } from '~/components/Points/RankItems'
-import { usePointStatusQuery } from '~/features/Points/PointStatus.query'
-import { useWallet } from '@solana/wallet-adapter-react'
-import { OpaqueDefault } from '~/components/Overview/OpaqueArea'
-import { useWalletDialog } from '~/hooks/useWalletDialog'
 
 const MyPointStatus = () => {
-  const { publicKey } = useWallet()
-  const { setOpen } = useWalletDialog()
-
-  const { data: infos, refetch } = usePointStatusQuery({
-    userPubKey: publicKey,
-    refetchOnMount: "always",
-    enabled: publicKey != null
-  })
 
   return (
     <Wrapper>
-      <Stack direction='row' gap={2}>
-        <BorderBox width='176px'>
+      <Stack direction='row' gap={16}>
+        <Box>
           <Box display='flex' justifyContent='center' alignItems='center'>
             <Typography variant='p'>Global Rank</Typography>
           </Box>
           <StatusValue>
-            {infos && <RankIndex rank={infos.myRank} />}
+            <RankIndex rank={1} />
           </StatusValue>
-        </BorderBox>
-        <BorderBox width='350px' position='relative'>
+        </Box>
+        <Box>
           <Box display='flex' justifyContent='center' alignItems='center'>
             <Typography variant='p'>Your Total Points</Typography>
             <InfoTooltip title={TooltipTexts.points.totalPoints} color='#66707e' />
           </Box>
           <StatusValue>
             <Typography variant='p_xlg'>
-              {infos && infos.totalPoints.toLocaleString()}
+              {/* {infos && infos.totalLiquidity > 0 ? `$${infos.totalLiquidity.toLocaleString()}` : '$0'} */}
             </Typography>
           </StatusValue>
-          <ClickBox>Click me</ClickBox>
-        </BorderBox>
+        </Box>
       </Stack>
-      <Stack direction='row' gap={2} mt='18px'>
-        <BorderBox width='250px'>
+      <Stack direction='row' gap={16}>
+        <Box>
           <Box display='flex' justifyContent='center' alignItems='center'>
             <Typography variant='p'>Your LP Points</Typography>
             <InfoTooltip title={TooltipTexts.points.lpPoints} color='#66707e' />
           </Box>
           <StatusValue>
             <Typography variant='p_xlg'>
-              {infos && infos.lpPoints.toLocaleString()}
+              {/* {infos && infos.totalLiquidity > 0 ? `$${infos.totalLiquidity.toLocaleString()}` : '$0'} */}
             </Typography>
           </StatusValue>
-        </BorderBox>
-        <BorderBox width='250px'>
+        </Box>
+        <Box>
           <Box display='flex' justifyContent='center' alignItems='center'>
             <Typography variant='p'>Your Trade Points</Typography>
             <InfoTooltip title={TooltipTexts.points.tradePoints} color='#66707e' />
           </Box>
           <StatusValue>
             <Typography variant='p_xlg'>
-              {infos && infos.tradePoints.toLocaleString()}
+              {/* {infos && infos.totalCollValue > 0 ? `$${infos.totalCollValue.toLocaleString()}` : '0'} */}
             </Typography>
           </StatusValue>
-        </BorderBox>
-        <BorderBox width='250px'>
+        </Box>
+        <Box>
           <Box display='flex' justifyContent='center' alignItems='center'>
             <Typography variant='p'>Your Social Points</Typography>
             <InfoTooltip title={TooltipTexts.points.socialPoints} color='#66707e' />
           </Box>
           <StatusValue>
             <Typography variant='p_xlg'>
-              {infos && infos.socialPoints.toLocaleString()}
+              {/* {infos && infos.totalCollValue > 0 ? `$${infos.totalCollValue.toLocaleString()}` : '0'} */}
             </Typography>
           </StatusValue>
-        </BorderBox>
+        </Box>
       </Stack>
-      {!publicKey && <>
-        <OpaqueDefault />
-        <ConnectWallet onClick={() => setOpen(true)}><Typography variant='p_xlg'>Connect Wallet</Typography></ConnectWallet>
-      </>}
     </Wrapper>
   )
 
@@ -95,45 +78,14 @@ const Wrapper = styled(Box)`
   margin-top: 16px;
   margin-bottom: 28px;
   padding: 12px 28px;
+  border-radius: 10px;
+  border: solid 1px ${(props) => props.theme.basis.jurassicGrey};
 `
 const StatusValue = styled(Box)`
   display: flex;
   justify-content: center;
   align-items: center;
-  margin-top: 22px;
-`
-const BorderBox = styled(Box)`
-  border-radius: 10px;
-  border: solid 1px rgba(255, 255, 255, 0.1);
-  padding-top: 14px;
-  padding-bottom: 22px;
-`
-const ClickBox = styled(Box)`
-  position: absolute;
-  top: 0;
-  right: 0;
-  font-size: 10px;
-  font-weight: 500;
-  margin: 4px 6px;
-  cursor: pointer;
-  color: ${(props) => props.theme.basis.slug};
-`
-const ConnectWallet = styled(Button)`
-  position: absolute;
-  top: 20px;
-  width: 360px;
-  height: 52px;
-  margin: 87px 213px;
-  object-fit: contain;
-  border-radius: 5px;
-  box-shadow: 0 0 15px 0 #005874;
-  border: solid 1px ${(props) => props.theme.basis.liquidityBlue};
-  background-color: #000;
-  color: #fff;
-  &:hover {
-    background-color: #000;
-    border-color: ${(props) => props.theme.basis.gloomyBlue}};
-  }
+  height: 80px;
 `
 
 export default MyPointStatus
