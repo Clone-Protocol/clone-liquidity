de0da14b7d329d6ca3b215aac9fbafb2bd0fac73
brian <99372590+qtum34@users.noreply.github.com>
2023-12-21 11:29:24 +0900

    Revert "update configure"
    This reverts commit 5a81d13ee08e4cf846be87779304f5dd0fa0b77c.

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,9 +8,6 @@ export async function middleware(req: NextRequest, context: Context) {
     'AF', 'AS', 'AO', 'AM', 'AZ', 'BY', 'BA', 'BW', 'BI', 'KH', 'CM', 'CA', 'CF', 'TD', 'CN', 'KP', 'CD', 'ER', 'ET', 'GH', 'GU', 'GN', 'GW', 'HT', 'IR', 'IQ', 'JP', 'LA', 'LB', 'LR', 'LY', 'MG', 'ML', 'MZ', 'MM', 'NI', 'MP', 'PK', 'PR', 'CG', 'RU', 'SO', 'SD', 'UA', 'LK', 'SY', 'TJ', 'TT', 'TM', 'UG', 'US', 'UZ', 'VU', 'VE', 'VI', 'YE', 'ZW'
   ]
 
-  console.log('contextGeo', context.geo)
-  console.log('geo', geo?.country)
-
   const countryCode = context.geo?.country?.code;
   const countryName = context.geo?.country?.name;
   // console.log('geo.country', geo?.country)
@@ -18,15 +15,15 @@ export async function middleware(req: NextRequest, context: Context) {
   console.log('countryName', countryName)
   // url.searchParams.set('country', geo?.country)
 
-  if (BLOCKED_COUNTRY_CODE_ARRAY.includes(countryCode)) {
-    return NextResponse.json({
-      result: false
-    })
-  } else {
-    return NextResponse.json({
-      result: true
-    })
-  }
+  // if (BLOCKED_COUNTRY_CODE_ARRAY.includes(countryCode)) {
+  //   return NextResponse.json({
+  //     result: false
+  //   })
+  // } else {
+  return NextResponse.json({
+    result: true
+  })
+  // }
 }
 
 export const config = {
