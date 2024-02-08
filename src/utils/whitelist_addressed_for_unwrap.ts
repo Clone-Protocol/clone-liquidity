1d14b93a365a0210cc3e64f78718298a6adaf997
dmark1415 <92603541+dmark1415@users.noreply.github.com>
2024-02-08 15:06:32 -0600

    Update whitelist_addressed_for_unwrap.ts

--- src/utils/whitelist_addressed_for_unwrap.ts
+++ src/utils/whitelist_addressed_for_unwrap.ts
@@ -1,9 +1,10 @@
 export const WHITELIST_ADDRESSES_FOR_UNWRAP = [
   'FjkKvFpazSfZXNw5JG3cKvkdQzTSmMuTyj9P37uuMapg',
   '3b1BL7uyGWPpsNJoX2hyBe8TkcYEXJU8oB3qcwX3Ygo2',
-  '4yyaBRYouqPjURvmwZcwcCiB6sQjPtxvzqJ4yDQTCPt9',
+  '4yyaBRYouqPjURvmwZcwcCiB6sQjPtxvzqJ4yDQTCPt9',Ï
   'HAME72nSC1pSyi9aYYzbSVkfT2ckvSbwGw2YXBWNyE8X',
   '2KKr4eTLLBWQy88G7mNufNQG7ghw23HHMxVUan7Cv1hV',
   '7Fq28FWhTdBHZd6ibs1TABMWZDUTJaJ9qB2hxkRHdXFD',
-  'GAbNoD8buH33yvfXoXuZQkdytr3j6t1eXRBXFiGKesYY'
+  'GAbNoD8buH33yvfXoXuZQkdytr3j6t1eXRBXFiGKesYY',
+  '4fmALpLi3GqFKRFvM2YWCy3PwePaX7HxAosnQsEkTCWQ'
 ]
