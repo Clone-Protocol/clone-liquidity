1e6887ef12a8d5718eb2f24ba5b8db9a471eec9d
brian <brian@initiumlabs.org>
2024-08-13 09:40:26 +0900

    change link

--- src/components/Common/PoolStatus.tsx
+++ src/components/Common/PoolStatus.tsx
@@ -23,7 +23,7 @@ export const PoolStatusButton = ({ status }: { status: Status }) => {
 
 export const FrozenButton = () => {
   return (
-    <a href="https://docs.clone.so/clone-mainnet-guide/pool-status" target="_blank">
+    <a href="https://cloneprotocol.medium.com/6ff85310b458" target="_blank">
       <CustomButton sx={{ backgroundColor: 'rgba(255, 142, 79, 0.2)', ':hover': { backgroundColor: 'rgba(255, 142, 79, 0.35)' } }}>
         <Image src={WarningFrozen} alt='frozen' /> <Typography variant='p'>Frozen</Typography>
       </CustomButton>
@@ -33,7 +33,7 @@ export const FrozenButton = () => {
 
 export const ExtractionButton = () => {
   return (
-    <a href="https://docs.clone.so/clone-mainnet-guide/pool-status" target="_blank">
+    <a href="https://cloneprotocol.medium.com/6ff85310b458" target="_blank">
       <CustomButton sx={{ backgroundColor: 'rgba(79, 229, 255, 0.2)', ':hover': { backgroundColor: 'rgba(79, 229, 255, 0.35)' } }}>
         <Image src={WarningExtraction} alt='extraction' /> <Typography variant='p'>Extraction</Typography>
       </CustomButton>
@@ -43,7 +43,7 @@ export const ExtractionButton = () => {
 
 export const LiquidationButton = () => {
   return (
-    <a href="https://docs.clone.so/clone-mainnet-guide/pool-status" target="_blank">
+    <a href="https://cloneprotocol.medium.com/6ff85310b458" target="_blank">
       <CustomButton sx={{ backgroundColor: 'rgba(237, 37, 37, 0.2)', ':hover': { backgroundColor: 'rgba(237, 37, 37, 0.35)' } }}>
         <Image src={WarningLiquidation} alt='frozen' /> <Typography variant='p'>Liquidation</Typography>
       </CustomButton>
